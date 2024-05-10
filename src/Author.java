29cad4c102042bbc6ddf08100a96016fbadb6107
Pavel <pavelpulle1@gmail.co>
2024-05-10 23:23:56 +0300

    Домашние задание 2

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
 
     private String firstName;
@@ -15,6 +17,25 @@ public class Author {
     public String getLastName() {
         return lastName;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+
+    @Override
+    public boolean equals(Object a) {
+        if (this == a) {
+            return true;
+        }
+        if (a == null || this.getClass() != a.getClass()) {
+            return false;
+        }
+        Author author = (Author) a;
+        return Objects.equals(firstName, author.firstName) && Objects.equals(lastName, author.lastName);
+    }
+
 
     @Override
     public String toString() {
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private String title;
@@ -26,6 +28,23 @@ public class Book {
         this.yearOfPublishing = yearOfPublishing;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) return false;
+        {
+
+        }
+        Book book = (Book) o;
+        return yearOfPublishing == book.yearOfPublishing && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearOfPublishing);
+    }
 
     @Override
     public String toString() {
